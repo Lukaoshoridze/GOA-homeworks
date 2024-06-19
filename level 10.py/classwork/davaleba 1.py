fff75ad543eb5f7a350ee559e998ba6ad5e1b792
Lukaoshoridze <chvritidzetamuna@gmail.com>
2024-06-19 11:53:26 +0400

    commit

---
+++ level 10.py/classwork/davaleba 1.py
@@ -0,0 +1,8 @@
+#ჩემს შესახებ
+my_name = "Luka"
+my_surname = "oshoridze"
+my_age = 10
+#დაპრინტვა
+print("my_name" + my_name)
+print("my_surname" + my_age)
+print("my_name" + my_surname + "my_age")
