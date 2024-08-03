2da83ab674808f363db2c8411a13aeff121f551b
Lukaoshoridze <chvritidzetamuna@gmail.com>
2024-08-03 14:52:38 +0400

    commit

---
+++ level 24/homework/davaleba 1.py
@@ -0,0 +1,40 @@
+ #ივარჯიშეთ განვლილ მასალაზე, for loop & while loop - ზე სათითაოდ გააკეთეთ 10 მაგალითი. 
+
+
+for i in range(20):
+    print("goa")
+for i in range(75):
+    print("Luka")
+for i in  range(8):
+    print("oshoridze")
+for i in range(3):
+    print("cxeni")
+for i in range(39):
+    print("chanta")
+
+
+    counter = 8
+while counter < 34:
+    print(counter)
+    counter += 1
+
+    counter = 28
+while counter >12:
+    print(counter)
+    counter += 1
+
+
+    counter = 94
+while counter > 64:
+    print(counter)
+    counter += 1
+
+    counter = 584
+while counter > 6:
+    print(counter)
+    counter += 1
+    
+    counter = 1
+while counter < 8:
+    print(counter)
+    counter += 1
---
+++ level 24/homework/davaleba 3.py
@@ -0,0 +1,6 @@
+#გააკეთეთ იგივე დავალება მაგრამ for loop -ის მაგივარად გამოიყენეთ while loop -ი.
+
+counter = 28
+while counter >24:
+    print(counter)
+    counter += 2
