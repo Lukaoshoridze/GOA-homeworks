40aebf1e546b2ca3e35e9c79771ab38870e1c8a2
Lukaoshoridze <chvritidzetamuna@gmail.com>
2024-08-18 11:00:57 +0400

    commit

---
+++ level 28/homework/davaleba 1.py
@@ -0,0 +1,23 @@
+#for ციკლის გამოყენებით გამოიტანეთ მხოლოდ 5ის ჯერადი რიცხვები 1-100ის ჩათვლით
+
+
+
+
+for i in range(1,100):
+    print(1)
+
+
+for i in range(1,100):
+    print(2)
+
+
+for i in range(1,100):
+    print(3)
+
+
+for i in range(1,100):
+    print(3)
+
+
+for i in range(1,100):
+    print(4)
